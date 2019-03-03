25586d43d0c8bde25c1401c95e976df8cc316150
Mark Gould <m.gould16@gmail.com>
2019-03-03 21:14:59 +0000

    display navba on index page not homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def index
+  end
+
   def contact
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/home'
-  get 'pages/contact'
+  get 'pages/contact', as: :contact
   get 'pages/about'
+  get 'pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
